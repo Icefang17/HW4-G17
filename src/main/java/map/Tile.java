cbd5687eae35cb2cc76af80529e418a8dc73e198
Chris Drechsel <cpdkd3@mail.missouri.edu>
2021-10-16 02:55:20 -0500

    Heuristic stuff

--- src/main/java/map/Tile.java
+++ src/main/java/map/Tile.java
@@ -2,6 +2,8 @@ package map;
 
 import resource.Mark;
 
+import java.awt.*;
+
 public class Tile {
 
     private Mark value;
@@ -13,4 +15,60 @@ public class Tile {
     public void setValue(Mark value){this.value = value;}
 
     public Mark getValue() {return value;}
+
+    public static Point goTopLeft(int x, int y, int xBound, int yBound){
+        if(checkBounds(x--, y++, xBound, yBound))
+            return new Point(x--, y++)
+        return new Point(0, 0);
+    }
+
+    public static Point goTop(int x, int y, int xBound, int yBound){
+        if(checkBounds(x, y++, xBound, yBound))
+            return new Point(x, y++);
+        return new Point(0, 0);
+    }
+
+    public static Point goTopRight(int x, int y, int xBound, int yBound){
+        if(checkBounds(x++, y++, xBound, yBound))
+            return new Point(x++, y++);
+        return new Point(0, 0);
+    }
+
+    public static Point goLeft(int x, int y, int xBound, int yBound){
+        if(checkBounds(x--, y, xBound, yBound))
+            return new Point(x--, y);
+        return new Point(0, 0);
+    }
+
+    public static Point goRight(int x, int y, int xBound, int yBound){
+        if(checkBounds(x++, y, xBound, yBound))
+            return new Point(x++, y);
+        return new Point(0, 0);
+    }
+
+    public static Point goBotLeft(int x, int y, int xBound, int yBound){
+        if(checkBounds(--x, y--, xBound, yBound))
+            return new Point(x--, y--);
+        return new Point(0, 0);
+    }
+
+    public static Point goBot(int x, int y, int xBound, int yBound){
+        if(checkBounds(x, y--, xBound, yBound))
+            return new Point(x, y--);
+        return new Point(0, 0);
+    }
+
+    public static Point goBotRight(int x, int y, int xBound, int yBound){
+        if(checkBounds(x++, y--, xBound, yBound))
+            return new Point(x++, y--);
+        return new Point(0, 0);
+    }
+
+    private static boolean checkBounds(int x, int y, int xBound, int yBound){
+        if(x < 0 || x > xBound)
+            return false;
+        if(y < 0 || y > yBound)
+            return false;
+        return true;
+    }
 }
--- src/main/java/resource/UtilityValue.java
+++ src/main/java/resource/UtilityValue.java
@@ -2,7 +2,9 @@ package resource;
 
 import game.Player;
 import map.State;
+import map.Tile;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class UtilityValue {
@@ -21,7 +23,7 @@ public class UtilityValue {
 
         // Calculate open sides for players and then:
 
-        utilityValue =
+       /* utilityValue =
                 100 * [num of 2-open 3-streak for player]
                 - 10 * [num of 2-open 3-streak for opponent]
 
@@ -32,8 +34,98 @@ public class UtilityValue {
                 - 2 * [num 2-open 2-streak for opponent]
 
                 + [num 1-open 2-streak for player]
-                - [num 1-open 2-streak for opponent];
+                - [num 1-open 2-streak for opponent];*/
+
+        utilityValue =
+                100 * (findStreaks(state, player, 3, true))
+                - 10 * (findStreaks(state, opponent, 3, true))
+
+                + 100 * (findStreaks(state, player, 3, false))
+                - 5 * (findStreaks(state, opponent, 3, false))
+
+                + 2 * (findStreaks(state, player, 2, true))
+                - 2 * (findStreaks(state, opponent, 2, true))
+
+                + (findStreaks(state, player, 2, false))
+                - (findStreaks(state, opponent, 2, false));
 
         return utilityValue;
+    }
+
+    private static int findStreaks(State state, Player player, int streak, boolean open){
+        Tile tiles[][] = state.getTiles();
+        Point coordinates;
+        ArrayList<Point> found = new ArrayList<>();
+
+        for(int x = 0; x < tiles.length; x++){
+            for(int y = 0; y < tiles[0].length; y++){
+                if(tiles[x][y].getValue() == Mark.BLANK){
+                    for(int i = 0; i < 8; i++){
+                        coordinates = checkAdjacentTiles(x, y, tiles.length, tiles[0].length, i);
+                        int counter = 0;
+                        int newX = coordinates.x;
+                        int newY = coordinates.y;
+                        do{
+                            counter++;
+
+                            // Break if at edge of board
+                            if(newX == coordinates.x && newY == coordinates.y)
+                                break;
+
+                            // Repeat the directional operation
+                            coordinates = checkAdjacentTiles(newX, newY, tiles.length, tiles[0].length, i);
+                            newX = coordinates.x;
+                            newY = coordinates.y;
+
+                            // Add streak to found if looking for closed streak
+                            if(counter == streak && tiles[newX][newY].getValue() == player.getMark()){
+                                coordinates = checkAdjacentTiles(newX, newY, tiles.length, tiles[0].length, i);
+                                Mark mark = tiles[coordinates.x][coordinates.y].getValue();
+                                // If looking for an open streak
+                                if(open){
+                                    // Make sure the streak is open and hasn't already been found
+                                    if(mark == Mark.BLANK && !found.contains(coordinates))
+                                        found.add(new Point(x, y));
+                                }
+                                else{
+                                    // Make sure the streak isn't bigger than the search value
+                                    if(mark != player.getMark() && mark != Mark.BLANK)
+                                        found.add(new Point(x, y));
+                                    // Check if streak ended on an edge
+                                    else if(coordinates.x == newX && coordinates.y == newY)
+                                        found.add(new Point(x, y));
+                                    break;
+                                }
+                            }
+                        }while(counter < streak && tiles[newX][newY].getValue() == player.getMark());
+                    }
+                }
+            }
+        }
+        return found.size();
+    }
+
+    private static Point checkAdjacentTiles(int x, int y, int xBound, int yBound, int instruction){
+        switch(instruction){
+            case(0):
+                return Tile.goTopLeft(x, y, xBound, yBound);
+            case(1):
+                return Tile.goTop(x, y, xBound, yBound);
+            case(2):
+                return Tile.goTopRight(x, y, xBound, yBound);
+            case(3):
+                return Tile.goLeft(x, y, xBound, yBound);
+            case(4):
+                return Tile.goRight(x, y, xBound, yBound);
+            case(5):
+                return Tile.goBotLeft(x, y, xBound, yBound);
+            case(6):
+                return Tile.goBot(x, y, xBound, yBound);
+            case(7):
+                return Tile.goBotRight(x, y, xBound, yBound);
+            default:
+                System.out.println("Something went wrong in [utility]!\n");
+        }
+        return new Point(0, 0);
     }
 }
