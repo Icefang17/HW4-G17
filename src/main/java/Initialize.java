4ca1d6efd185013a8bafffff9438d94ef35a66c4
Chris Drechsel <cpdkd3@mail.missouri.edu>
2021-10-17 16:58:45 -0500

    Console prints ('user interface')

--- src/main/java/Initialize.java
+++ src/main/java/Initialize.java
@@ -17,15 +17,21 @@ public class Initialize {
 
         Game game = Game.newXOGame(initialState, 2);
         int count = 0;
+        long startTime = System.nanoTime();
         while(!Game.terminalTest(game.getGameState())){
             currentPlayer = game.getCurrentPlayer();
+            System.out.println("\nTurn: " + count);
             game.getGameState().printState();
             Point action = MiniMax.minimaxDecision(game, game.getGameState(), currentPlayer.getPlayerId() * 2);
+            if(count == 0)
+                System.out.println("First Move: X -> (" + action.x + ", " + action.y + ")");
             game.getGameState().setTileToPlayer(action.x, action.y);
             game.endTurn();
-            System.out.println(count);
             count++;
         }
+        long endTime = System.nanoTime();
+        System.out.println("Game ended with time: " + (((endTime - startTime) / 1000000000) / 60) + ":" + (((endTime - startTime) / 1000000000) % 60));
+        System.out.println("Player " + game.getCurrentPlayer().getPlayerId() + " won the game!");
         System.out.println("SOMEBODY WON?");
     }
 
